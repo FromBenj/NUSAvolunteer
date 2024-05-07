5e61c0cec3a09989f0541abccdce760693cef030
Benjamin TOUBOUL <touboul.benjamin@gmail.com>
2024-05-07 15:40:02 +0200

    Home page initialisation

--- assets/app.js
+++ assets/app.js
@@ -1,10 +1,13 @@
-import './bootstrap.js';
-/*
- * Welcome to your app's main JavaScript file!
- *
- * This file will be included onto the page via the importmap() Twig function,
- * which should already be in your base.html.twig.
- */
+
 import './styles/app.scss';
 
-console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
+import './js/home-title';
+const $ = require('jquery');
+// this "modifies" the jquery module: adding behavior to it
+// the bootstrap module doesn't export/return anything
+require('bootstrap');
+$(document).ready(function() {
+    $('[data-toggle="popover"]').popover();
+});
+
+console.log("It is working 🎉")
---
+++ assets/js/home-title.js
@@ -0,0 +1,37 @@
+import {gsap} from "gsap";
+import {SplitTextJS} from "./SplitTextJS";
+
+
+const titles = gsap.utils.toArray('.title-animation');
+const tl = gsap.timeline();
+
+titles.push("end")
+titles.forEach((title) => {
+    if (title !== "end") {
+    const splitTitle = new SplitTextJS(title);
+    tl
+        .from(splitTitle.chars, {
+            opacity: 0,
+            y: 80,
+            rotateX: -90,
+            stagger: .02,
+        }, "<")
+
+        .to(splitTitle.chars, {
+            opacity: 0,
+            y: -80,
+            rotateX: 90,
+            stagger: .02,
+        })
+    }
+})
+function afterAnimation() {
+    const container = document.getElementById('title-animation-container');
+    const navTitle = document.getElementById("anime-title");
+    container.remove();
+    navTitle.style.visibility = "visible";
+}
+tl.eventCallback("onComplete", afterAnimation);
+
+
+

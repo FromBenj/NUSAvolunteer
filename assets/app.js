802296b6c162a0952f2971060bc0c8451170ea12
Benjamin TOUBOUL <touboul.benjamin@gmail.com>
2024-07-04 13:02:15 +0200

    Style of the chat

--- assets/app.js
+++ assets/app.js
@@ -1,10 +1,14 @@
-
 import './styles/app.scss';
 
 import './js/home-title';
-import './js/lorem_orelsum.min';
 import './js/edit-scroll';
+
+// For the moment, directly declare on the organisation/search.html.twig file
 // import './js/match-button';
+
+// Transfer to public/js folder because it is only used in the home page
+//import './js/lorem_orelsum.min';
+
 const $ = require('jquery');
 import * as bootstrap from 'bootstrap';
 window.bootstrap = bootstrap;
--- assets/js/match-button.js
+++ assets/js/match-button.js
@@ -1,4 +1,4 @@
-function RevealStar(Id1, Id2){
+function revealStar(Id1, Id2){
     const btn1= document.getElementById(Id1);
     const btn2= document.getElementById(Id2);
     btn1.addEventListener("click", () => {
